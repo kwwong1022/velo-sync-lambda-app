aa4b2b433c90dbb2c2948a346d858bf15f412779
Kenneth Wong <kwwong2210@gmail.com>
2024-03-04 00:40:24 +0800

    Update aws region environment variable

--- src/util/lambdaClient.js
+++ src/util/lambdaClient.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const { LambdaClient } = require("@aws-sdk/client-lambda");
 
 const lambda = new LambdaClient({
-    region: process.env.AWS_REGION,
+    region: process.env.AWS_SERVICE_REGION,
 });
 
 module.exports = lambda
--- src/util/s3Client.js
+++ src/util/s3Client.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const { S3Client } = require("@aws-sdk/client-s3")
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_SERVICE_REGION,
 })
 
 module.exports = s3
